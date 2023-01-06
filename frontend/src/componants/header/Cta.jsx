08f4ae5ef0a36fbdf96d6c6ede2583fd54799fdf
Kishore N <kishoren2572@gmail.com>
2023-01-06 15:57:12 +0530

    Updated Resume

--- frontend/src/componants/header/Cta.jsx
+++ frontend/src/componants/header/Cta.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Cta = () => {
   return (
     <div className='cta' >
-      <a href="https://drive.google.com/file/d/10FyA1b5TZR9UDxI-d4d2XdeGByLUdTvM/view?usp=sharing" className='btn' target='_blank'>Download CV</a>
+      <a href="https://drive.google.com/file/d/1H60MDV_H9q0QcByFREQtFnXo7xApVK0a/view?usp=sharing" className='btn' target='_blank'>Download CV</a>
       <a href="#contact" className='btn btn-primary'>Let's Talk</a>
     </div>
   )
