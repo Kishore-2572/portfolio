2d9c6796f051e0551ece508298292e0c9c19a23c
Kishore N <kishoren2572@gmail.com>
2023-02-12 20:24:17 +0530

    Projects update

--- frontend/src/componants/portfolio/Portfolio.jsx
+++ frontend/src/componants/portfolio/Portfolio.jsx
@@ -10,11 +10,21 @@ const Portfolio = () => {
       <h5>My Recent Works</h5>
       <h2>Portfolio</h2>
       <div className="container portfolio-container">
+      <article className="portfolio-item">
+          <div className="portfolio-item-img">
+            <img src={salonbooking} alt="" />
+          </div>
+          <h3>Salon online appointment booking website (MERN stack)</h3>
+          <div className="portfolio-item-cta">
+            <a href="https://github.com/Kishore-2572/salon-booking" className='btn'  target='_blank' >Github</a>
+            <a href="https://zrog.onrender.com/" className='btn btn-primary' target='_blank' >Live Demo</a>
+          </div>
+        </article>
         <article className="portfolio-item">
           <div className="portfolio-item-img">
             <img src={amazona} alt="" />
           </div>
-          <h3>An ecommerce website developed with MERNstack</h3>
+          <h3>An simple ecommerce website  (MERN stack)</h3>
           <div className="portfolio-item-cta">
             <a href="https://github.com/Kishore-2572/mern-ecommerce" className='btn' target='_blank' >Github</a>
             <a href="https://amazona-yblw.onrender.com/" className='btn btn-primary' target='_blank' >Live Demo</a>
@@ -24,23 +34,12 @@ const Portfolio = () => {
           <div className="portfolio-item-img">
             <img src={mykec} alt="" />
           </div>
-          <h3>It is mobile application developed for my college staffs</h3>
+          <h3>It is mobile application developed for my college (Flutter)</h3>
           <div className="portfolio-item-cta">
             <a href="https://github.com/Kishore-2572/my-kec" className='btn' target='_blank' >Github</a>
             <a href="https://mykec.kongu.edu/IMAGES/app-release.apk" className='btn btn-primary' target='_blank' >Live Demo</a>
           </div>
         </article>
-        <article className="portfolio-item" title="Live Demo isn't available">
-          <div className="portfolio-item-img">
-            <img src={salonbooking} alt="" />
-          </div>
-          <h3>Salon Booking website developed with flask</h3>
-          <div className="portfolio-item-cta">
-            <a href="https://github.com/Kishore-2572/salon-booking" className='btn'  target='_blank' >Github</a>
-            <a href="https://zrog.onrender.com/" className='btn btn-primary' target='_blank' >Live Demo</a>
-          </div>
-        </article>
-       
       </div>
     </section>
   )
